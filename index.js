cc2bbd82d670eb778146fce3fd23c31e426d33f9
valderyjr <valderyjunior2002@gmail.com>
2022-02-01 19:13:02 -0300

    add caso video e input data min e max

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+    let today = new Date();
+    let dia = today.getDate();
+    let mes = today.getMonth() + 1; //January is 0!
+    let ano = today.getFullYear();
+    
+    if (dia < 10) {
+       dia = '0' + dia;
+    }
+    
+    if (mes < 10) {
+       mes = '0' + mes;
+    } 
+        
+    today = ano + '-' + mes + '-' + dia;
+    $('#date').attr('max', today);
+})
+
 $('#btn').click(function(){
     const data = $('#date').val();
     const valida = validaEntrada(data);
@@ -7,15 +25,26 @@ $('#btn').click(function(){
             method: "GET",
             url: `https://api.nasa.gov/planetary/apod?api_key=NqBxvsgm294FLGzK7ObEtTrcOQKp1wSlyu64wYmH&date=${data}`,
             success: function() {
-                const img = api.responseJSON.url;
+                const type = api.responseJSON.media_type;
+                const url = api.responseJSON.url;
                 const autor = api.responseJSON.copyright;
                 const title = api.responseJSON.title;
                 const explanation = api.responseJSON.explanation;
-                $('.content-img').removeClass('invisible');
-                $('#img').attr('src', img);
-                console.log(img);
-                $('.entrada').addClass('invisible');
+                if (type === 'image') {
+                    $('.content-img').removeClass('invisible');
+                    $('#img').removeClass('invisible');
+                    $('#img').attr('src', url);
+                    console.log(url);
+                } else {
+                    $('.content-img').removeClass('invisible');
+                    $('#video').removeClass('invisible');
+                    $('#video').attr('src', url);
+                    console.log(url);
+                }
+
+                $('.entrada').adiaClass('invisible');
                 $('.resp').removeClass('invisible');
+
                 $('#titulo-resp').html(title);
                 $('#texto-resp').html(explanation);
                 console.log(autor);
@@ -28,9 +57,9 @@ $('#btn').click(function(){
 })
 
 $('#btn-voltar').on('click', function (){
-    $('.content-img').addClass('invisible');
+    $('.content-img').adiaClass('invisible');
     $('.entrada').removeClass('invisible');
-    $('.resp').addClass('invisible');
+    $('.resp').adiaClass('invisible');
 })
 
 function validaEntrada(date) {
